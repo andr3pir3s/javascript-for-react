160cd5774bc6a08e5b335662517f4fb0a5600032
andre-pires <andr3victor@gmail.com>
2022-09-14 22:58:12 -0300

    master to main

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
-
-import CardGame from "./src/components/CardGame";
+import BoardGame from './src/objects/BoardGame';
 
 const $root = document.querySelector("#root");
+const $htmlBoardGame = BoardGame(4);
 
-const $htmlCardGame = CardGame();
-
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
